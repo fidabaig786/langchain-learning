4b60e901a5e25150154b90b55ddce1b97aeb67c4
fidabaig786 <alibaigfiver@gmail.com>
2026-02-27 19:46:45 +0100

    chatbot with openai api

---
+++ 5_chatbot/chatbot.py
@@ -0,0 +1,21 @@
+from langchain_openai import ChatOpenAI
+from dotenv import load_dotenv
+
+load_dotenv()
+
+model = ChatOpenAI()
+
+chat_history=[
+
+]
+
+while True:
+    user_input=input("You: ")
+    chat_history.append(user_input)
+    if user_input == 'exit':
+        break
+    response = model.invoke(chat_history)
+    chat_history.append(response.content)
+    print(response.content)
+
+    
