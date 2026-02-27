3f50de444e6fd5eca561019585fe39ee39560290
fidabaig786 <alibaigfiver@gmail.com>
2026-02-27 20:03:53 +0100

    added SystemMessage, AImessage and Humanmessage to the chatbot

--- 5_chatbot/chatbot.py
+++ 5_chatbot/chatbot.py
@@ -1,21 +1,23 @@
 from langchain_openai import ChatOpenAI
 from dotenv import load_dotenv
 
+from langchain_core.messages import SystemMessage, HumanMessage, AIMessage
+
 load_dotenv()
 
 model = ChatOpenAI()
 
 chat_history=[
-
+     SystemMessage(content='You are a good writer')
 ]
 
 while True:
-    user_input=input("You: ")
+    user_input=input(HumanMessage(content="You: "))
     chat_history.append(user_input)
     if user_input == 'exit':
         break
     response = model.invoke(chat_history)
-    chat_history.append(response.content)
-    print(response.content)
+    chat_history.append(AIMessage(content=response.content))
+    print("AI: ", response.content)
 
-    
+
